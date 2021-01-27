ba3a353fed5f0d7ae1b3a14479c8d39eb04ef443
Sungpil Han <pil.han@gmail.com>
2021-01-27 13:42:49 +0900

    Added a elective course

--- get-stable.sh
+++ get-stable.sh
@@ -1,14 +1,49 @@
 hugo mod clean
 
-#v5.0.0-beta.0
-hugo mod get github.com/wowchemy/wowchemy-hugo-modules/netlify-cms-academic@be0fa17ac006d36d2a1691d3760f43abce6f6327
-hugo mod get github.com/wowchemy/wowchemy-hugo-modules/wowchemy@be0fa17ac006d36d2a1691d3760f43abce6f6327
+#hugo mod get github.com/wowchemy/wowchemy-hugo-modules/netlify-cms-academic@
+#hugo mod get github.com/wowchemy/wowchemy-hugo-modules/wowchemy@
 
-#v5.0.0-beta.1
+#v5.0.0-beta.0 # 2020-10-22 # Successful
+#hugo mod get github.com/wowchemy/wowchemy-hugo-modules/netlify-cms-academic@be0fa17ac006d36d2a1691d3760f43abce6f6327
+#hugo mod get github.com/wowchemy/wowchemy-hugo-modules/wowchemy@be0fa17ac006d36d2a1691d3760f43abce6f6327
+
+# 2020-10-29 # Successful
+#hugo mod get github.com/wowchemy/wowchemy-hugo-modules/netlify-cms-academic@ab5b005820d1c9f830acf36b4c2d8f9f916c1071
+#hugo mod get github.com/wowchemy/wowchemy-hugo-modules/wowchemy@ab5b005820d1c9f830acf36b4c2d8f9f916c1071
+
+# 2020-11-07 # Successful
+#hugo mod get github.com/wowchemy/wowchemy-hugo-modules/netlify-cms-academic@eb76e56841f832c639f6278c2bd47696320e7f2e
+#hugo mod get github.com/wowchemy/wowchemy-hugo-modules/wowchemy@eb76e56841f832c639f6278c2bd47696320e7f2e
+
+# 2020-11-10 # Successful
+#hugo mod get github.com/wowchemy/wowchemy-hugo-modules/netlify-cms-academic@1ed538027a753ab794dfd7da93e10323c3f625f1
+#hugo mod get github.com/wowchemy/wowchemy-hugo-modules/wowchemy@1ed538027a753ab794dfd7da93e10323c3f625f1
+
+# 2020-11-11 # https://github.com/wowchemy/wowchemy-hugo-modules/commit/f21a0cf8f44fceee9a253ef9a2749b2f1c7e55b9
+hugo mod get github.com/wowchemy/wowchemy-hugo-modules/netlify-cms-academic@f21a0cf8f44fceee9a253ef9a2749b2f1c7e55b9
+hugo mod get github.com/wowchemy/wowchemy-hugo-modules/wowchemy@f21a0cf8f44fceee9a253ef9a2749b2f1c7e55b9
+
+# 2020-11-11 # Fail
+#hugo mod get github.com/wowchemy/wowchemy-hugo-modules/netlify-cms-academic@6ff8f5c3ee9df6cf1995ad70fbd09ebef4413fee
+#hugo mod get github.com/wowchemy/wowchemy-hugo-modules/wowchemy@6ff8f5c3ee9df6cf1995ad70fbd09ebef4413fee
+
+# 2020-11-11 # Fail
+#hugo mod get github.com/wowchemy/wowchemy-hugo-modules/netlify-cms-academic@165dcd6c3eabcdd16acd77b17bba787f0eb49613
+#hugo mod get github.com/wowchemy/wowchemy-hugo-modules/wowchemy@165dcd6c3eabcdd16acd77b17bba787f0eb49613
+
+# 2020-11-12 # Fail
+#hugo mod get github.com/wowchemy/wowchemy-hugo-modules/netlify-cms-academic@05f70db04134e7f55d4da72e8bfbc37e724b184e
+#hugo mod get github.com/wowchemy/wowchemy-hugo-modules/wowchemy@05f70db04134e7f55d4da72e8bfbc37e724b184e
+
+# 2020-11-14 # Fail
+#hugo mod get github.com/wowchemy/wowchemy-hugo-modules/netlify-cms-academic@9ee65a120fe8a83832e66bdd025e15f84c0b32ab
+#hugo mod get github.com/wowchemy/wowchemy-hugo-modules/wowchemy@9ee65a120fe8a83832e66bdd025e15f84c0b32ab
+
+#v5.0.0-beta.1 # 2020-12-05 # Fail
 #hugo mod get github.com/wowchemy/wowchemy-hugo-modules/netlify-cms-academic@5fa9412f9e766b70bdf66956ac1570eb2915e748
 #hugo mod get github.com/wowchemy/wowchemy-hugo-modules/wowchemy@5fa9412f9e766b70bdf66956ac1570eb2915e748
 
-#v5.0.0-beta.2
+#v5.0.0-beta.2 # 2021-01-20 # Fail
 #hugo mod get github.com/wowchemy/wowchemy-hugo-modules/netlify-cms-academic@be0fa17ac006d36d2a1691d3760f43abce6f6327
 #hugo mod get github.com/wowchemy/wowchemy-hugo-modules/wowchemy@be0fa17ac006d36d2a1691d3760f43abce6f6327
 
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/wowchemy/starter-academic
 go 1.15
 
 require (
-	github.com/wowchemy/wowchemy-hugo-modules/netlify-cms-academic v0.0.0-20201016182837-be0fa17ac006
-	github.com/wowchemy/wowchemy-hugo-modules/wowchemy v0.0.0-20201016182837-be0fa17ac006
+	github.com/wowchemy/wowchemy-hugo-modules/netlify-cms-academic v0.0.0-20201110231724-f21a0cf8f44f
+	github.com/wowchemy/wowchemy-hugo-modules/wowchemy v0.0.0-20201110231724-f21a0cf8f44f
 )
